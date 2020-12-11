17b0751db2cf75c20273003eef2e7dcdf70667f2
Jonathon Terry <jonathonterry93@gmail.com>
2020-12-11 11:00:21 -0700

    Bootstrap erudite-core app

---
+++ src/main/java/com/jonathonterry/eruditecore/EruditeCoreApplication.java
@@ -0,0 +1,13 @@
+package com.jonathonterry.eruditecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EruditeCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EruditeCoreApplication.class, args);
+	}
+
+}
